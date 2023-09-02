90b15bcdc3c0df2572da45b66c919a9ea619d70f
Ranjithdnv <rith8596@gmail.com>
2023-09-02 20:48:29 +0530

    socket

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    setSocket(io("https://sock-hepv.onrender.com:5000"));
+    setSocket(io("https://sock-hepv.onrender.com"));
   }, []);
 
 
